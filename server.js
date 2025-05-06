d5d970cc647df11f57c72c4998a9b7e027f688dc
FilipLiljenberg <filipliljenberg9@.com>
2025-05-06 13:21:26 +0200

    merging Filip with main

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoutes from "./routes/auth.js";
-=======
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import authRoutes from './routes/auth.js';
->>>>>>> 8f2c29c (updates)
 
 dotenv.config();
 
