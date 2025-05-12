1f7d5201a45e804324e59ad29b7da5f8d6516693
FilipLiljenberg <filipliljenberg9@.com>
2025-05-12 10:46:39 +0200

    merge error, pushing to main

--- server.js
+++ server.js
@@ -11,44 +11,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api", authRoutes); // <--- viktigt!
-
-const users = [
-  { name: "Anders", password: "hej123" },
-  { name: "Karl", password: "hej1" },
-];
-
-app.post("/login", async (req, res) => {
-  const user = req.body;
-  const { name, password } = user;
-
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].name === name) {
-      if (users[i].password === password) {
-        return res.json(user);
-      } else {
-        return res.status(401).json({ message: "Wrong password" });
-      }
-    }
-  }
-
-  res.status(404).json({ message: "User not found" });
-});
-
-app.get("/test", async (req, res) => {
-  console.log("test!");
-  res.send("API is working!");
-});
+app.use("/api", authRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(PORT, () => console.log("Server is up and runnning!"));
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err.message);
-  });
+  .connect("mongodb://localhost:27017/twitter")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () =>
+  console.log(`Server is up and running really fast on port ${PORT}!`)
+);
