ab3463a08c5b829d6e55ba304c3c3629dc84fb00
Jetsetradioh <jetsetradioh@protonmail.com>
2025-05-12 11:56:47 +0200

    user.js router.get /username + server.js import routes och app.use

--- routes/user.js
+++ routes/user.js
@@ -1,1 +1,21 @@
 //Profiler, vänner? followers?
+import express from "express";
+import User from "../models/User.js";
+
+const router = express.Router();
+
+router.get("/username", async (req, res) => {
+  const { username } = req.params;
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ message: "Användare hittades inte" });
+    }
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: "Serverfel" + err.message });
+  }
+});
+
+export default router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/user.js";
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", authRoutes);
+app.use("/api", userRoutes);
 
 mongoose
   .connect("mongodb://localhost:27017/twitter")
