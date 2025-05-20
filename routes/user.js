f682756ee1afe7df4bcd1832b9f06a7f8bba29be
FilipLiljenberg <filipliljenberg9@.com>
2025-05-20 15:36:53 +0200

    man kan nu se users profiler och tweets

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,22 @@
 //Profiler, vänner? followers?
 import express from "express";
 import User from "../models/User.js";
+import Tweet from "../models/Tweet.js";
 
 const router = express.Router();
+
+//för att hämta alla users i postman
+router.get("/users", async (req, res) => {
+  const users = await User.find().lean();
+  res.json(users);
+});
+
+//visa profilen man klickar på
+router.get("/user/:id", async (req, res) => {
+  const user = await User.findById(req.params.id);
+  const tweets = await Tweet.find({ userId: req.params.id });
+  res.json([user, tweets]);
+});
 
 router.get("/:username", async (req, res) => {
   const { username } = req.params;
